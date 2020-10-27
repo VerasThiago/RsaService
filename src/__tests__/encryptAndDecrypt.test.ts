b145dede4fb8f36e93f6e0d27a53dede9df6d5da
Thiago Veras <thiago.veras.machado@gmail.com>
2020-10-27 14:44:36 -0300

    Lint

--- src/__tests__/encryptAndDecrypt.test.ts
+++ src/__tests__/encryptAndDecrypt.test.ts
@@ -1,4 +1,4 @@
-import { RsaService } from '../module/rsaService';
+import { RsaService } from '../module/rsaService'
 
 const SECRET_ALGORITHM: string = 'aes-256-ctr'
 const SECRET_KEY: string = 'FoCKvdLslUuB4y3EZlKate7XGottHski1LmyqJHvUhs'
@@ -8,6 +8,6 @@ it('Testing hello world', async () => {
   expect.assertions(1)
   const rsa: RsaService = RsaService.getSingleton(SECRET_ALGORITHM, SECRET_KEY, SECRET_IV_LENGTH)
   const plainTextBefore: string = 'Hello World'
-  const plainTextAfet: string =  rsa.decrypt(rsa.encrypt(plainTextBefore))
+  const plainTextAfet: string = rsa.decrypt(rsa.encrypt(plainTextBefore))
   expect(plainTextBefore).toEqual(plainTextAfet)
 })
